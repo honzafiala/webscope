48b10d6f00adfe60be467f824c543feaee38775c
honza <honza.fiala97@gmail.com>
2022-10-22 01:15:27 +0200

    Revert "Created branch with dev-lowlevel example code"
    This reverts commit 0afb8156f5f72ba2fffcadc85d337a282ee0cc39.

---
+++ driver.c
@@ -0,0 +1,196 @@
+// SPDX-License-Identifier: CC0-1.0
+
+#include "tusb_option.h"
+
+#include "device/usbd_pvt.h"
+#include "pico/stdlib.h"
+
+
+
+
+#define USBTEST_LOG1    printf
+#define USBTEST_LOG2    printf
+
+
+uint8_t _ctrl_req_buf[256];
+
+//#define BULK_BUFLEN    (32 * 1024)
+#define BULK_BUFLEN    (64)
+
+static uint8_t _bulk_in;
+static uint8_t _bulk_out;
+
+static uint8_t _bulk_in2;
+static uint8_t _bulk_out2;
+
+
+uint8_t _bulk_in_buf[BULK_BUFLEN];
+uint8_t _bulk_out_buf[BULK_BUFLEN];
+
+extern volatile int buf_ready;
+
+#define CAPTURE_DEPTH (96*1024)
+
+extern uint8_t capture_buf[CAPTURE_DEPTH];
+extern uint8_t capture_buf2[CAPTURE_DEPTH];
+
+extern uint usb_pin;
+
+volatile uint data_ready = 0;
+
+extern volatile uint trig_index;
+static uint xfer_count = 0;
+
+extern volatile bool xfer_started;
+
+
+static void usbtest_init(void)
+{
+    USBTEST_LOG1("%s:\n", __func__);
+
+
+}
+
+static void usbtest_reset(uint8_t rhport)
+{
+    (void) rhport;
+
+    USBTEST_LOG1("%s: rhport%u\n", __func__, rhport);
+}
+
+static void usbtest_disable_endpoint(uint8_t rhport, uint8_t *ep_addr)
+{
+    if (*ep_addr) {
+        usbd_edpt_close(rhport, *ep_addr);
+        *ep_addr = 0;
+    }
+}
+
+static uint16_t usbtest_open(uint8_t rhport, tusb_desc_interface_t const * itf_desc, uint16_t max_len)
+{
+    USBTEST_LOG1("%s: bInterfaceNumber=%u max_len=%u\n", __func__, itf_desc->bInterfaceNumber, max_len);
+
+    TU_VERIFY(TUSB_CLASS_VENDOR_SPECIFIC == itf_desc->bInterfaceClass, 0);
+
+    uint16_t const len = sizeof(tusb_desc_interface_t) + itf_desc->bNumEndpoints * sizeof(tusb_desc_endpoint_t);
+    TU_VERIFY(max_len >= len, 0);
+
+    usbtest_disable_endpoint(rhport, &_bulk_in);
+    usbtest_disable_endpoint(rhport, &_bulk_out);
+
+
+
+
+    uint8_t const * p_desc = tu_desc_next(itf_desc);
+
+    TU_ASSERT( usbd_open_edpt_pair(rhport, p_desc, 2, TUSB_XFER_BULK, &_bulk_out, &_bulk_in) );
+
+    TU_ASSERT ( usbd_edpt_xfer(rhport, _bulk_out, _bulk_out_buf, sizeof(_bulk_out_buf)) );
+    TU_ASSERT ( usbd_edpt_xfer(rhport, _bulk_in, _bulk_in_buf, 0));
+
+
+    return len;
+}
+
+extern void handle_usb_in(uint32_t xferred_bytes, uint8_t * buf);
+
+volatile bool xfer_complete = false;
+
+volatile bool data_sent = false;
+uint8_t volatile * buf_in;
+volatile uint buf_in_size;
+
+void usb_send(uint8_t * buf, uint size) {
+    printf("usb_send %d\n", buf_in_size);
+    data_sent = false;
+    buf_in = buf;
+    buf_in_size = size;
+    while (!data_sent);
+    buf_in_size = 0;
+}
+
+
+static bool usbtest_control_xfer_cb(uint8_t rhport, uint8_t stage, tusb_control_request_t const * req)
+{
+    uint16_t wLength = tu_min16(req->wLength, sizeof(_ctrl_req_buf));
+    int ret;
+
+    USBTEST_LOG2("%s:  bRequest=0x%02x bmRequestType=0x%x %s wLength=%u(%u) stage=%u\n",
+                 __func__, req->bRequest, req->bmRequestType,
+                 req->bmRequestType_bit.direction ? "IN" : "OUT", wLength, req->wLength, stage);
+
+    if (stage != CONTROL_STAGE_SETUP)
+        return true;
+
+    switch (req->bRequest) {
+
+    // Used by test #9
+    // FIXME: tinyusb core should handle this
+    case TUSB_REQ_GET_STATUS:
+        if (req->bmRequestType_bit.type != TUSB_REQ_TYPE_STANDARD ||
+            req->bmRequestType_bit.recipient != TUSB_REQ_RCPT_INTERFACE ||
+            req->bmRequestType_bit.direction != TUSB_DIR_IN)
+            return false;
+
+        _ctrl_req_buf[0] = 0;
+        _ctrl_req_buf[1] = 0;
+        wLength = tu_min16(wLength, 2);
+        USBTEST_LOG2("TUSB_REQ_GET_STATUS: intf=%u\n", req->wIndex);
+        break;
+
+    default:
+        USBTEST_LOG2("REQ not recognised (core might handle it)\n");
+        return false;
+    }
+
+    return tud_control_xfer(rhport, req, _ctrl_req_buf, wLength);
+}
+
+static bool usbtest_xfer_cb(uint8_t rhport, uint8_t ep_addr, xfer_result_t result, uint32_t xferred_bytes)
+{
+    USBTEST_LOG1("%s: ep_addr=0x%02x result=%u xferred_bytes=%u\n", __func__, ep_addr, result, xferred_bytes);
+
+
+    TU_VERIFY(result == XFER_RESULT_SUCCESS);
+
+    static uint xfer_index; 
+    // if (!xferred_bytes) USBTEST_LOG2("                 ZLP\n");
+
+    if (ep_addr == _bulk_out) {
+       //handle_usb_in(xferred_bytes, _bulk_out_buf);
+       data_ready = xferred_bytes;
+        TU_ASSERT ( usbd_edpt_xfer(rhport, _bulk_out, _bulk_out_buf, sizeof(_bulk_out_buf)) );
+    }
+
+    else if (ep_addr == _bulk_in) {
+        usbd_edpt_xfer(rhport, _bulk_in, buf_in, buf_in_size);
+        printf("sending %d bytes\n", buf_in_size);
+        data_sent = true;     
+    }
+    else
+        return false;
+
+    return true;
+}
+
+static usbd_class_driver_t const _usbtest_driver[] =
+{
+    {
+  #if CFG_TUSB_DEBUG >= 2
+        .name             = "usbtest",
+  #endif
+        .init             = usbtest_init,
+        .reset            = usbtest_reset,
+        .open             = usbtest_open,
+        .control_xfer_cb  = usbtest_control_xfer_cb,
+        .xfer_cb          = usbtest_xfer_cb,
+        .sof              = NULL
+    },
+};
+
+usbd_class_driver_t const* usbd_app_driver_get_cb(uint8_t* driver_count)
+{
+	*driver_count += TU_ARRAY_SIZE(_usbtest_driver);
+
+	return _usbtest_driver;
+}
---
+++ main.c
@@ -0,0 +1,191 @@
+// SPDX-License-Identifier: CC0-1.0
+
+#include "bsp/board.h"
+#include "tusb.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
+// For ADC input:
+#include "hardware/adc.h"
+#include "hardware/dma.h"
+
+#include "pico/multicore.h"
+
+extern void core1_task();
+
+volatile bool xfer_started = false;
+
+extern volatile bool xfer_complete;
+
+#define CAPTURE_CHANNEL 0
+#define CAPTURE_DEPTH (96*1024)     
+
+uint8_t capture_buf[CAPTURE_DEPTH] = {128};
+uint8_t capture_buf2[CAPTURE_DEPTH] = {128};
+uint8_t lol[1024];
+
+uint dma_chan, dma_chan2;
+
+volatile dma_channel_config cfg, cfg2;
+
+uint dma_pin = 16;
+uint dma_pin2 = 17;
+uint usb_pin = 18;
+
+volatile int buf_ready = -1;
+extern volatile int dma_finished;
+
+uint trig = -1;
+volatile uint trig_index;
+
+extern void usb_send(uint8_t * buf, uint size);
+
+void dma_irq(int dma_num) {
+    gpio_put(dma_num ? dma_pin2 : dma_pin, 1);
+
+    // Reset IRQ flag
+    int cur_dma_chan = dma_num ? dma_chan2 : dma_chan;
+    dma_hw->ints1 = (1u << cur_dma_chan);
+
+    // Set buffer ready flag so the data from this DMA transfer can be sent over USB
+    buf_ready = dma_num;
+
+    // Check the buffer for trigger condition
+    if (trig == -1) {
+        uint8_t * finished_buf = dma_num ? capture_buf2 : capture_buf;
+        for (int i = 0; i < CAPTURE_DEPTH; i++) {
+            if (finished_buf[i] > 200) {
+                trig = dma_num;
+                trig_index = i;
+                break;
+            } 
+        }
+    }
+
+    uint capture_depth;
+    if (trig == dma_num) {
+        capture_depth = trig_index;
+        channel_config_set_chain_to(dma_num ? &cfg2 : &cfg, dma_num);
+    }
+    else capture_depth = CAPTURE_DEPTH;
+
+    // Reconfigure the DMA channel
+    uint8_t * buf = dma_num ? capture_buf2 : capture_buf;
+    dma_channel_configure(dma_num ? dma_chan2 : dma_chan, dma_num ? &cfg2 : &cfg,
+        buf,    // dst
+        &adc_hw->fifo,  // src
+        capture_depth,  // transfer count
+        false            // start immediately
+    );
+    gpio_put(dma_num ? dma_pin2 : dma_pin, 0);
+
+}
+
+void dma1_irq() {dma_irq(0);}
+void dma2_irq() {dma_irq(1);}
+
+void dma_channels_init_config() {
+
+}
+
+int main(void)
+{
+    board_init();
+
+    multicore_launch_core1(core1_task);
+
+    gpio_init(dma_pin);
+    gpio_set_dir(dma_pin, GPIO_OUT);
+    gpio_init(dma_pin2);
+    gpio_set_dir(dma_pin2, GPIO_OUT);
+    gpio_init(usb_pin);
+    gpio_set_dir(usb_pin, GPIO_OUT);
+
+    // Configure ADC
+    adc_gpio_init(26 + CAPTURE_CHANNEL);
+    adc_gpio_init(26 + CAPTURE_CHANNEL + 1);
+    adc_select_input(CAPTURE_CHANNEL);
+    adc_init();
+    //  adc_set_round_robin(0x3);
+    adc_fifo_setup(
+        true,    // Write each completed conversion to the sample FIFO
+        true,    // Enable DMA data request (DREQ)
+        1,       // DREQ (and IRQ) asserted when at least 1 sample present
+        false,   // We won't see the ERR bit because of 8 bit reads; disable.
+        true     // Shift each sample to 8 bits when pushing to FIFO
+    );
+    adc_set_clkdiv(0);
+
+    printf("\n\nArming DMA\n");
+    // Claim two DMA channels
+    dma_chan = dma_claim_unused_channel(true);
+    dma_chan2 = dma_claim_unused_channel(true);
+    printf("DMA channels %d %d\n", dma_chan, dma_chan2);
+
+            // Configure DMA 1
+    cfg = dma_channel_get_default_config(dma_chan);
+    channel_config_set_transfer_data_size(&cfg, DMA_SIZE_8);
+    channel_config_set_read_increment(&cfg, false);
+    channel_config_set_write_increment(&cfg, true);
+    // Enable IRQ 0 for DMA 1
+    dma_channel_set_irq0_enabled(dma_chan, true);
+    irq_set_exclusive_handler(DMA_IRQ_0, dma1_irq);
+    irq_set_enabled(DMA_IRQ_0, true);
+    // Chain DMA2 to DMA1
+    channel_config_set_chain_to(&cfg, dma_chan2);
+    channel_config_set_dreq(&cfg, DREQ_ADC);
+    dma_channel_configure(dma_chan, &cfg,
+        capture_buf,    // dst
+        &adc_hw->fifo,  // src
+        CAPTURE_DEPTH,  // transfer count
+        false            // start immediately
+    );
+
+
+    // Configure DMA 2
+    cfg2 = dma_channel_get_default_config(dma_chan2);
+    channel_config_set_transfer_data_size(&cfg2, DMA_SIZE_8);
+    channel_config_set_read_increment(&cfg2, false);
+    channel_config_set_write_increment(&cfg2, true);
+    // Enable IRQ 1 for DMA 2
+    dma_channel_set_irq1_enabled(dma_chan2, true);
+    irq_set_exclusive_handler(DMA_IRQ_1, dma2_irq);
+    irq_set_enabled(DMA_IRQ_1, true);
+    // Chain DMA1 to DMA2
+    channel_config_set_chain_to(&cfg2, dma_chan);
+    channel_config_set_dreq(&cfg2, DREQ_ADC);
+    dma_channel_configure(dma_chan2, &cfg2,
+        capture_buf2,    // dst
+        &adc_hw->fifo,  // src
+        CAPTURE_DEPTH,  // transfer count
+        false            // start immediately
+    );
+
+
+    gpio_put(dma_pin, 1);
+    gpio_put(dma_pin2, 1);
+    dma_start_channel_mask(1u << dma_chan);
+    gpio_put(dma_pin, 0);
+    gpio_put(dma_pin2, 0);
+
+    sleep_ms(500);
+
+    printf("Starting capture\n");
+     adc_run(true);
+
+
+
+    while (1) {
+        while (dma_channel_is_busy(dma_chan) || dma_channel_is_busy(dma_chan2));
+        printf("DMA finished\n");
+        uint32_t trig_msg = trig_index;
+        usb_send((uint8_t * ) &trig_msg, 4);
+        printf("trigger sent\n");
+        usb_send(capture_buf, 32768);
+
+        while (1);
+    }
+
+    while (1);
+
+    return 0;
+}
