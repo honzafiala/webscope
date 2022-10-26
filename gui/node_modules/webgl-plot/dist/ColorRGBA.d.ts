30567bbac11dae559269fe8d0387ac12fff5aebc
honza <honza.fiala97@gmail.com>
2022-10-26 16:53:18 +0200

    asfe

--- gui/node_modules/webgl-plot/dist/ColorRGBA.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ColorRGBA {
-    r: number;
-    g: number;
-    b: number;
-    a: number;
-    constructor(r: number, g: number, b: number, a: number);
-}
-//# sourceMappingURL=ColorRGBA.d.ts.map
--- gui/node_modules/webgl-plot/dist/ColorRGBA.js
+++
@@ -1,9 +0,0 @@
-export class ColorRGBA {
-    constructor(r, g, b, a) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
-        this.a = a;
-    }
-}
-//# sourceMappingURL=ColorRGBA.js.map
