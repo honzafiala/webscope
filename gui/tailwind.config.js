ca12b7d27e9c4668cc0df1c8c5e01f02ebd8798b
honza <honza.fiala97@gmail.com>
2022-11-30 23:29:49 +0100

    Succesfully configured TailWind

--- gui/tailwind.config.js
+++ gui/tailwind.config.js
@@ -1,13 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
