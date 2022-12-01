9d1276f690f1d4b84c8681ffe8a9dc46d3ba95c4
honza <honza.fiala97@gmail.com>
2022-12-01 11:34:35 +0100

    tailwind_experiments

--- gui/src/SplashScreen.js
+++ gui/src/SplashScreen.js
@@ -1,7 +1,21 @@
 export default function SplashScreen() {
     return (
-        <div className="absolute z-50 flex items-center justify-center h-screen">
-       <div>Kokos</div>
-      </div>
+<div className="absolute grid place-items-center h-screen w-screen">
+
+<div className="py-8 px-8 mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <div className="text-center space-y-2 sm:text-left">
+    <div className="space-y-0.5">
+      <p className="text-lg text-black font-semibold">
+        Welcome to Webscope
+      </p>
+
+    </div>
+    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Connect device</button>
+    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Download firmware</button>
+    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Help/About</button>
+  </div>
+</div>
+
+</div>
     );
 }
